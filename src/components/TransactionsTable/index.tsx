3ac374bbf1c1c1ad81c10c0667a11d04bbd818ef
Arlisson Costa <Arlisson.fs13@gmail.com>
2022-07-13 22:27:31 -0300

    Primeiro commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  useEffect(() => {
+    api.get("transactions").then((data) => console.log(data));
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>10/07/2022</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$1.100</td>
+            <td>Casa</td>
+            <td>06/07/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
